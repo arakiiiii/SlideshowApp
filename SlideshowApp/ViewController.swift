a74c95f8f3536bdc38ac450d28a646f273528894
Akira Kamite <akirakamite@akiranomacbook-puro.local>
2018-11-13 12:46:03 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Akira Kamite on 2018/11/13.
+//  Copyright © 2018 araki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
